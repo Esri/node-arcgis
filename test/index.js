ecfa97aa317fe1fce70fc80e32fa5cd2f6b29388
Nikolas Wise <nikolaswise@gmail.com>
2015-10-29 14:03:36 -0700

    restructure api a bit

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var ArcGIS = require('../src/index')
 
-var token = ''
+var token = 'r4ckNoUtPoM8y52SjLzA-KkmYrxeKsNZ_K4dHYCmY-2h8B_oDtrBsdclYwem2rcQJk-c5Ipg92yRmJJtVXVVqWA5DYp6kHxdFXRI6x0CmRxVw-uyXBWpRWTlPsA_A3rEZ7PJscZ8yOxnQ_VhOb2_fg..'
 
 var ago = ArcGIS(token)
 window.ago = ago
