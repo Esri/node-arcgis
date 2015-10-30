f48ce927c2b69ca4f0bc2ddbd63750b007bf44b5
Nikolas Wise <nikolaswise@gmail.com>
2015-10-30 13:14:42 -0700

    more doc

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
 
 var ArcGIS = require('../src/index')
 
-var token = 'r4ckNoUtPoM8y52SjLzA-KkmYrxeKsNZ_K4dHYCmY-2h8B_oDtrBsdclYwem2rcQJk-c5Ipg92yRmJJtVXVVqWA5DYp6kHxdFXRI6x0CmRxVw-uyXBWpRWTlPsA_A3rEZ7PJscZ8yOxnQ_VhOb2_fg..'
+var token = ''
 
 var ago = ArcGIS(token)
 window.ago = ago
