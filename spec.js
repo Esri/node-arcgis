145e55a0d2b1c2ff10682a39a53c52be08cf4e70
Nikolas Wise <nikolaswise@gmail.com>
2015-10-30 09:22:02 -0700

    fix commas in spec

--- spec.js
+++ spec.js
@@ -14,7 +14,7 @@ let client = token => {
       form.f     = 'pjson'
       return rq.get(`${rootUrl}${url}`, form)
     },
-    getUser: function(options) {console.log('gets a user')}
+    getUser: function(options) {console.log('gets a user')},
     user: {
       new: function(options){console.log('creates a new user via an inivtation?')},
       update: function(options){console.log('updates the user information')},
@@ -24,27 +24,27 @@ let client = token => {
       enable: function(){console.log('enables a disabled user')},
       disable: function(){console.log('disables a user')}
     },
-    getOrganization: function(options) {console.log('gets an organization')}
+    getOrganization: function(options) {console.log('gets an organization')},
     organization: {
       update: function(options) {console.log('updates org information')},
-      users: function() {console.log('gets users in an org')}
-      content: function() {console.log('gets content in an org')}
-      summary: function() {console.log('gets the short summary of an org')}
+      users: function() {console.log('gets users in an org')},
+      content: function() {console.log('gets content in an org')},
+      summary: function() {console.log('gets the short summary of an org')},
       deleteUsers: function (options) {console.log('deletes users from an org')}
     },
-    getGroup: function(options) {console.log('gets a group')}
+    getGroup: function(options) {console.log('gets a group')},
     group: {
       new: function(){console.log('creates a new group')},
       update: function(options){console.log('updates group information')},
       delete: function(){console.log('deletes a group')},
-      content: function() {console.log('gets the content in a group')}
+      content: function() {console.log('gets the content in a group')},
       users: function(options){console.log('adds a user to a group, via invitation if needed. If options is null, returns users in a group')},
       removeUser: function(options){console.log('kicks a user out of a group')},
       join: function(options){console.log('request an invitation for a user to a group')},
       leave: function(options){console.log('leave a group')},
       changeOwner: function(){console.log('changes the owner of a group?')}
     },
-    getItem: function(options) {console.log('gets an item')}
+    getItem: function(options) {console.log('gets an item')},
     item: {
       new: function(){console.log('creates a new item')},
       update:  function(options) {console.log('updates an items information')},
@@ -56,16 +56,16 @@ let client = token => {
       changeOwner:  function (options) {console.log('changes the owner of an item')},
       favorite: function () {console.log('adds item to favorites')},
       currentRating: function () {console.log('gets current item rating')},
-      rate: function () {console.log('adds rating to item')}
+      rate: function () {console.log('adds rating to item')},
       unrate: function(){console.log('removes rating from an item')},
       publish: function () {console.log('publishes a static data item to a usable layer')},
       export: function(){console.log('exports item as selected data type')},
-      data: function(){console.log('gets the data behind an item')}
+      data: function(){console.log('gets the data behind an item')},
       deleteProtected: function () {console.log('sets delete protection status')},
       register:  function () {console.log('registers an item as an application')},
       oAuth:  function () {console.log('returns oauth information for an app')},
       relatedItems:  function () {console.log('returns items related to an item')},
-      permissions: function (options) {console.log('sets permissions on an item to self, org, or public. If options is null, returns current permissions')},
+      permissions: function (options) {console.log('sets permissions on an item to self, org, or public. If options is null, returns current permissions')}
       comments: {
         all: function() {console.log('returns all comments on an item')},
         get: function(options){console.log('returns a particular comment on an item')},
@@ -82,15 +82,15 @@ let client = token => {
       delete: function(){console.log('Deletes a set of items')},
       getTags: function(options) {console.log('gets all the tags in an array of items')}
     },
-    getUsage: function() {console.log('gets usage information')}
+    getUsage: function() {console.log('gets usage information')},
     usage: {
-      getSummary: function() {console.log('gets a summary of org usage')}
-      stypeToService: function(options) {console.log('turns usage types into human readable words')}
-      parseProduct: function(options) {console.log('figures out what usage api is trying to tell you')}
-      flatten: function(options) {console.log('squishes response from usage api into graphable data')}
+      getSummary: function() {console.log('gets a summary of org usage')},
+      stypeToService: function(options) {console.log('turns usage types into human readable words')},
+      parseProduct: function(options) {console.log('figures out what usage api is trying to tell you')},
+      flatten: function(options) {console.log('squishes response from usage api into graphable data')},
       periodToMs: function(options) {console.log('figured out what a reporting period even is')}
     },
-    getBilling: function(options) {console.log('gets billing information')}
+    getBilling: function(options) {console.log('gets billing information')},
     billing: {
       status: function(){console.log('checks status of billing')}
     },
