a1f189ce2f1551e381f6d2b5cc93c4d102e615f3
Nikolas Wise <nikolaswise@gmail.com>
2015-11-13 09:49:57 -0800

    item.rate() returns updated item object

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var ArcGIS = require('../lib/index')
 // user token
-var token = ''
+var token = 'ZNYx0lboOS-P5ezBDJtPoJwidCKJKlYw5owCYXo4N8_N99fxCu6TmXFCWcQWVa2ClGdvczB9kiTHpPQgxsRuVKwvxDTAXiTqjpHYnGg3pdiWEUs28fqLmkkFCqR5-mohJWnIWTBkb6QPslVGvn3QGudqbqWtGaVXY_nd5dEIf4beAc0x4syArlPqbfTxiI_1'
 
 var ago = ArcGIS({
   token: token
