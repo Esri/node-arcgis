6d136ac81a34e9a9b52b36fd3df8100a753161bd
Nikolas Wise <nikolaswise@gmail.com>
2015-12-04 10:22:47 -0800

    item update working great

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var ArcGIS = require('../lib/index')
 // user token
-var token = '7G4dfp3P7KSzd0eTJBYUN4Johe1SoqUUychQnZKle8wMtwJhzZLwTbCPRc1RB4mqvNIKNY0aWEk6fiwf34ACBsv8k4znGyG9cMj1Z7WPK6W33LxIdfQjlLK7ys7DDGh5lAUVai3QDNVGa3K-Hm8q13yXmFI7_YQpL6Rx9LHBqtb29P3olcrwC_pZXTpG1R4q'
+var token = 'kiggNVka9n91SCUNfg3YBQpPFu6_YR0kS3lhgmZJs-foBcgr1lxuZN9_kRZOXXwtdKAVFmb8IT0_HwEGwa2dWs6qzFPspB-haZxY3KZr9qj4JWHPYw2aAOaoBQQV_83fIeZullmDiIyRSI2EB7Q9HVLlh11vr0hWsyU64TlVN1H8uoYcfg4t-2153qYYVo_H'
 
 var ago = ArcGIS({
   token: token
