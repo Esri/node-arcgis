fe4a6153a65afcaa7b6e1404bd242a6238c410d9
Nikolas Wise <nikolaswise@gmail.com>
2015-12-08 14:31:48 -0800

    folder ... kinda working?

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var ArcGIS = require('../lib/index')
 // user token
-var token = 'okOk9ZVAlx2_msS-wxIKcGbdqGj0XLDnoHdYRZ3ptv01ygoztJaEw-urlfh-w8SmLm19gGNQN6y-bC7-KjhPOHViBxcWh1zYh5hRptFZD1-yLDuyC4nh_R2M7wxm9X7lVRafeV-iMQXiNS8c8rjQCWYuESRqhqyTPgAm5FPY3XeZ4Cz0vgBR0SZvaSjRw-5Y'
+var token = '_VFPz4bDcwNtiU6TmZaP_QwQxeW4Ol1DwVQZatIe7xjXD2MCBliwcvODm17eRvNcE0mO1pqwrptWaPtkGrc5yckYeyPVoAqdeGdlqVgHs9Ma5nR29lUI2cTaPDng6_5DpOTinDvzkrnuifXL9notVlovS1-MpC0NYmHFBPHIi5anbFG2scNd0v-2Vv4X7B9A'
 
 var ago = ArcGIS({
   token: token
