e9ec5da643e7c224cf91e2d763d262a47e36c83c
Nikolas Wise <nikolaswise@gmail.com>
2015-12-08 13:12:15 -0800

    favorite/unfavorite

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var ArcGIS = require('../lib/index')
 // user token
-var token = 'kiggNVka9n91SCUNfg3YBQpPFu6_YR0kS3lhgmZJs-foBcgr1lxuZN9_kRZOXXwtdKAVFmb8IT0_HwEGwa2dWs6qzFPspB-haZxY3KZr9qj4JWHPYw2aAOaoBQQV_83fIeZullmDiIyRSI2EB7Q9HVLlh11vr0hWsyU64TlVN1H8uoYcfg4t-2153qYYVo_H'
+var token = 'yEmvesxDwl7YDiMchjgPErYahOYsjudZgL38i3y_Fjb8fNTVF00IHuGVH9B1i0RKgKLTDB5MjO1ZHgtfa6OQrEcENmAqR45hEmGM_logtMjwtM_hAtDIJQGgYv7z3g-0VjKvKhWg9GqnBC2qTZCe8-lDWHY56Wb3ec5dH95DLrJWnktsaYEwMzR-2HxE9Hsb'
 
 var ago = ArcGIS({
   token: token
