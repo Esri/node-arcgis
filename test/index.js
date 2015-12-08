bb6c271ace5780f1f3c3225ea36e5d27dcf6a723
Nikolas Wise <nikolaswise@gmail.com>
2015-12-08 13:26:17 -0800

    item groups

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var ArcGIS = require('../lib/index')
 // user token
-var token = 'yEmvesxDwl7YDiMchjgPErYahOYsjudZgL38i3y_Fjb8fNTVF00IHuGVH9B1i0RKgKLTDB5MjO1ZHgtfa6OQrEcENmAqR45hEmGM_logtMjwtM_hAtDIJQGgYv7z3g-0VjKvKhWg9GqnBC2qTZCe8-lDWHY56Wb3ec5dH95DLrJWnktsaYEwMzR-2HxE9Hsb'
+var token = 'okOk9ZVAlx2_msS-wxIKcGbdqGj0XLDnoHdYRZ3ptv01ygoztJaEw-urlfh-w8SmLm19gGNQN6y-bC7-KjhPOHViBxcWh1zYh5hRptFZD1-yLDuyC4nh_R2M7wxm9X7lVRafeV-iMQXiNS8c8rjQCWYuESRqhqyTPgAm5FPY3XeZ4Cz0vgBR0SZvaSjRw-5Y'
 
 var ago = ArcGIS({
   token: token
