cdb58c1551c2d50ab0b604a47e79e9bb27295d24
Nikolas Wise <nikolaswise@gmail.com>
2015-12-10 11:18:44 -0800

    app oauth and app token methods

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var ArcGIS = require('../lib/index')
 // user token
-var token = 'XP-SgwoFOmrHQAa_sTHzNzWQw5hU5JAF6K_YKF14gUaM4GHyWpMkKC5t-68f5dkH7CN5M-LradtpVOrGgycqduEFQGOgAgBMHjXpsnhLyzBKYVEKYDc5RKR2rJ0r2zzTQn3oI8wkkuWoBpH_4DOg534KRRCIMhr3EMHoTHmq3ICbK_C1s40RnvpWSl4sOJ-p'
+var token = 'Me8POZJgER_sFYshVKyO7WYKnW91CpbIArGPeTvjblEw6E0MCVQZk9BxBbHunpmTQn5S8X9EMud518FjHd-F8KUGOqefY9cwkQTsHtZM1Kj_wPfEYDE1vwf_nLtrdzkW2eLwxRCN9NavBnKYSLgoxOU4AG69Amm-O2wucTo98JP1K85b6vwfkAOfLUjy7Hty'
 
 var ago = ArcGIS({
   token: token
