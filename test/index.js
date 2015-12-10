fea0bd4c5c7221f1c8c52146920d71d1095cf96d
Nikolas Wise <nikolaswise@gmail.com>
2015-12-10 14:39:28 -0800

    delete item

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var ArcGIS = require('../lib/index')
 // user token
-var token = 'mO1-NtBJdtvo0TuOmR6XB0fwdWUpNgRvJ_kU7EW14JDLONslj7IqMSarB28RpO5fkzPVsCP3xxGNdU-_XE7InUiMCaBLr6rIBX9X411je5qVWpNl3LYlPA1ggykonCaEamIbFZUeHyGzHuS5tPKqJ8WnA3NBhrvcGtAMmrlWdOxAamxzNQxvL4DPE-GeTpyA'
+var token = 'ZJCtNJ3b7CrPvdiFCtl95gqIzC9VG_abNJicyYw9HOVU6wgTRv3OWRMiHtU_DpXDwwdqm7sCAgreVMcGaDlTVVIhtVXKeBxjADuhhXZm6CFVKnY9vRq9UoxhMGM4P-_yM-INLl44wlqWsWl8iREuZRmY4M2HqJRdQubL60H0ixQwpCy4oCORWDt5BRvaISlr'
 
 var ago = ArcGIS({
   token: token
