791bab0c3b650baba890fcfbe948ce9e0100b7a5
Nikolas Wise <nikolaswise@gmail.com>
2015-12-10 10:20:20 -0800

    Delete protection, very cool

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var ArcGIS = require('../lib/index')
 // user token
-var token = '_VFPz4bDcwNtiU6TmZaP_QwQxeW4Ol1DwVQZatIe7xjXD2MCBliwcvODm17eRvNcE0mO1pqwrptWaPtkGrc5yckYeyPVoAqdeGdlqVgHs9Ma5nR29lUI2cTaPDng6_5DpOTinDvzkrnuifXL9notVlovS1-MpC0NYmHFBPHIi5anbFG2scNd0v-2Vv4X7B9A'
+var token = 'XP-SgwoFOmrHQAa_sTHzNzWQw5hU5JAF6K_YKF14gUaM4GHyWpMkKC5t-68f5dkH7CN5M-LradtpVOrGgycqduEFQGOgAgBMHjXpsnhLyzBKYVEKYDc5RKR2rJ0r2zzTQn3oI8wkkuWoBpH_4DOg534KRRCIMhr3EMHoTHmq3ICbK_C1s40RnvpWSl4sOJ-p'
 
 var ago = ArcGIS({
   token: token
