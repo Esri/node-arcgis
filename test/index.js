640b8398a0a258e2215b3d964c09c22dcda9ed78
Nikolas Wise <nikolaswise@gmail.com>
2015-12-16 09:14:47 -0800

    related items parsing results and returning single structured array

--- test/index.js
+++ test/index.js
@@ -1,7 +1,6 @@
 var ArcGIS = require('../lib/index')
 // user token
-var token = 'MegHBNBqQv3zKWaUYIS_KisX9aj7Lm94GMWBrmt5zhTBsmxceBbE6R4vKLtQoMzEmb8LQnXX77yUN6MK2TITrFtBlm6E_ezb0IgmwYGuhUT9Tz-GQe_koNwKANPGUZUKH7d-FJa9mBF7idBaY561LbSCYFWfJmjiAPVAsk9wtNhNl2_4FThfLNJuKraSENAu'
-
+var token = 'vKt363TZ2IPm0VYbf-gxQsZ3G6AVBaXFZlNzUG17PIPzjSJOAxTI-yfLigie8wZUxKp-yOHRD-dVXwwZcO65Jm7KNX4l9qfi-BO-v1Dg2lO6shPaXo1unTMFWIYK-EP1R3Hd5auei9x1oUTTbWVsD5UbGy1BRkx9Q1gH_nWnmhJy0EgyOZ9UVYt0w3xoRWkH'
 var ago = ArcGIS({
   token: token
 })
