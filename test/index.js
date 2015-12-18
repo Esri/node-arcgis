b73b707b90f55e02452b936fe6c55ba8bc313cd6
Nikolas Wise <nikolaswise@gmail.com>
2015-12-18 08:50:59 -0800

    rough in an error handling module

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var ArcGIS = require('../lib/index')
 // user token
-var token = 'm_agvy9xI89aIRIUu_DLfbnDPPtSFqNqtzHaPe50xH6R4Z8avX95sIJwJbkvdoZorgvpwVFrLAKbSAt-m3g7etmA7HKOYwA2BoCtLOagY76Rmu6Decir5FJpkAw_Ch79ITD6m4T-o9C7oXCTXyTUqqUQBFfWDu2vOT7hRAA60od9njinCFBVzsBOZNUb44Ic'
+var token = '18fmralCU3wxRGzjiKMOcilGArkJf2s5Qy4LsDlqiInOWN0PRvoR8xGpO8lgZopHgAcwYHEY7Dn40eXcmzEShPa68m8nxh4diA6m6BF3jHhxg9M-iJWTI043lup4v71PXUfD7fvRRM8ukmnNqP8XKY9b_D0KNZiYzbHtEBvoek_hPZXScwb7z3qFgf8E5OEk'
 var ago = ArcGIS({
   token: token
 })
