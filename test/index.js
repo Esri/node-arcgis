b23f9e6813ce7cd9bcdb0d78831f6fb040e6257e
Nikolas Wise <nikolaswise@gmail.com>
2016-01-05 15:34:09 -0800

    stabalize docs a bit

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var ArcGIS = require('../lib/index')
 // user token
-var token = 'wkBB62iGAl-hSKvYcIir2_twDcxM3v3nbTAECcUDLODVFQ4ixmCC7Qn3kSIAQB3NevRNXgc858jTJLMVT7_Z_Lbwqs-29ivw-WinyeDOgbUJOvuklnsUC0_pzfzU14fvUlKvmCgfDStqdWA3spvoTdUVDZgto5ZZ_bsRePgVceohIbkCan6G9U5JUaU6EwDV'
+var token = 'DIiRVsWLpnRrNWUZqCtHNa0ju9z7cvOe00Vj1aGP9isvALHXRFwJ7liuJCxcuZs-Az8Iq8DsEsnsCey5WYz9wbBAKMgWTqO2ad5J_6BR6xRt37dVxMlBPoy1W-iv26b0PtxBjaXIW9nKFjsUBTg-JL3pgpDlkMRU0JzxfOweCx39IY96jhE37cTV_yZcEGjx'
 var ago = ArcGIS({
   token: token
 })
