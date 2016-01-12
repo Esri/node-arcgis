58169a6d43deb2bc934cc8447daad20205440bcb
Nikolas Wise <nikolaswise@gmail.com>
2016-01-12 14:06:29 -0800

    strip down the browser file to not handle auth for dev

--- source/browser.js
+++ source/browser.js
@@ -1,9 +1,1 @@
-var ArcGIS = require('../lib/index')
-// user token
-var token = 'v8jabHd4zK2a3GIQnqAFOSXVEVVwmlI4FZYXYEIxzcpofbPEUgwZkE08MtGbhlEoHwAa-BbnNHd6vhdt_ygiHCXYK_o5irb9E1cGW0R6iLfVxc47zNU_Btgz0p9xigkZAxCXVlVck7SyvjBeV0EgsUa61yEzZttER-Wce2_SvKqCSXjEL4ObsuKibLkSqsZ3'
-var ago = ArcGIS({
-  token: token
-})
-
-window.ago = ago
-
+window.ArcGIS = require('../lib/index')
