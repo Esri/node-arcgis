2f2a9dd9f17e9e80d9df85069c7659e42c7302a9
Nikolas Wise <nikolaswise@gmail.com>
2016-01-15 17:40:28 -0800

    like one millions docs

--- source/browser.js
+++ source/browser.js
@@ -1,9 +1,8 @@
 var ArcGIS = require('../lib/index')
 // user token
-var token = 'UTD9kxIdz_g18U7OZMyV_3YX2bndlY4bN4uOSUqXawKApN5NIm9NgJDZ3-R6Fse5Tz2vWJGsvlMrl53FLc1JBysfnRtOhJ8jg_6yIKHd3GLtdCO47ljpuM8zqVCpiBurmMTI0zUI4w4yOpPMnOx4FydiVZbY1Rwj3B_Ad2HObKShmptivFdPUkmhZwTfHhPy'
+var token = '0aNOLhXZHTkgIajYytcID4QAEigs5M8B3fvFmjFbl78atxWgh4-BXhVHJgrxK_BrO1kvGV3YbadziB3B4jLA0bCzUDkfBvq5mYV-e-r61EtW0La6IashIqzjvtlDtmXsRzH3jT6QdwXEtsK1xaComomb7GWG_y2sgLTt7vd9wyC7rrhvBpzMyg91E29ZZVqA'
 var ago = ArcGIS({
   token: token
 })
 
 window.ago = ago
-
