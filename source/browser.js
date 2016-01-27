a58479497788e34952f78941a53978032b08e1a9
Nikolas Wise <nikolaswise@gmail.com>
2016-01-27 12:52:19 -0800

    item data

--- source/browser.js
+++ source/browser.js
@@ -1,6 +1,6 @@
 var ArcGIS = require('../lib/index')
 // user token
-var token = '7addTS7UxjMqYJ_eFInoWq96H8YlIefeAKQpT9rgM_Bq7A4nC1K-taa_bq8JfQZwM_NO2Pd63V7gooCNm1BYCEXzRC8eEOoIwKTvV808fhSnDFqef2Zp7RX0w6VHcBZqAKLi-dti-d-ro2UoKHUSVpKGqr1E8_6MC54azYdG_JAtBn0OlwObGZ1peMdntcWX'
+var token = 'vVqiJyAUmQAFAktbkYhMkwJwJY6iubq51iJLjUdCkfzgXtxAAqKemcQu1cpaDdCaLVHEditpa2PF5HQxlBf2zjf4HjSu8jMngMp40i0m5TjevhDyNf9afLL2uaTN6bjWX5k7Uk-3mgZThGnbZlpV2GHLssFyOGXZh9W0Ctd9w7LSsGVU-GnXt1I44Gw2G9Hh'
 var ago = ArcGIS({
   token: token
 })
