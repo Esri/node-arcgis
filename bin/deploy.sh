8ee35502084806f96d995ff1decd7da35901b3d8
Nikolas Wise <nikolaswise@gmail.com>
2016-04-04 09:31:21 -0700

    tweak deploy scripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 git branch -D gh-pages
 git checkout -b gh-pages
-npm install
 npm run doc -- --PRD
 cp -r docs/ ./
-rm -r node_modules
-git add -f *
+git add -f index.html
+git add -f css/**.*
+git add -f js/**.*
+git add -f fonts/**.*
+git add -f img/**.*
+git add -f calcite-ui.*
 git commit -m "deploy"
 git push origin gh-pages -f
 git reset --hard HEAD~1
