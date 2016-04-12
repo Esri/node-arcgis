329557f3396f8ffaefce066c8f3e244cce33bf43
Nikolas Wise <nikolaswise@gmail.com>
2016-04-12 09:32:34 -0700

    tweak script to push subtree to gh pages

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,15 +2,9 @@
 git branch -D gh-pages
 git checkout -b gh-pages
 npm run doc -- --PRD
-cp -r docs/ ./
-git add -f index.html
-git add -f css/**.*
-git add -f js/**.*
-git add -f fonts/**.*
-git add -f img/**.*
-git add -f calcite-ui.*
+git add -f doc-built/
 git commit -m "deploy"
-git push upstream gh-pages -f
+git subtree push --prefix doc-built upstream gh-pages -f
 git reset --hard HEAD~1
 git checkout master
 git branch -D gh-pages
